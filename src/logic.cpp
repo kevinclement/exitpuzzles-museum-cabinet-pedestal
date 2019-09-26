7e0ea2aa16990869aea0dd10ccfc494771e4731c
Kevin Clement <kevin.clement@gmail.com>
2019-09-25 21:39:50 -0700

    rfid

--- src/logic.cpp
+++ src/logic.cpp
@@ -3,11 +3,10 @@
 #include "consts.h"
 
 unsigned long solved_at = 0;
-bool stopped_all = false;
 
 Logic::Logic() 
   : serial(),
-    actuator(*this),
+    rfid(*this),
     lights(*this),
     lightsensors(*this),
     sound(*this)
@@ -17,44 +16,21 @@ Logic::Logic()
 void Logic::setup() {
   serial.setup("ExitMuseumMummy");
 
-  actuator.setup();
+  rfid.setup();
   lights.setup();
   lightsensors.setup();
   sound.setup();
-
-  // trigger a close, if its already closed then noop
-  // if this is a reset, then close here will do the actual close
-  actuator.close();
 }
 
 void Logic::handle() {
   serial.handle();
   lightsensors.handle();
   lights.handle();
-  actuator.handle();
-
-  if (lightsensors.lightDetected && solved_at == 0) {
-    solved();
-  }
-
-  // don't start the flashing until a few seconds to sync with audio
-  if (!stopped_all && !lights.lights_on && solved_at > 0 && millis() - solved_at > 2800) {
-    lights.on();
-  }
-
-  if (!stopped_all && solved_at != 0 && actuator.opened) {
-    serial.print("All done with lights and music!\n");
-    sound.stop();
-    lights.off();
-    stopped_all = true;
-  }
+  rfid.handle();
 }
 
 void Logic::solved() {
   serial.print("Solved!\n");
-
-  sound.play();
-  actuator.open();
 
   solved_at = millis();
   status();
--- src/main.cpp
+++ src/main.cpp
@@ -19,29 +19,13 @@ void solve(int) {
   logic.solved();
 }
 
-void lights(int) {
-  logic.serial.print("toggling lights...%s", CRLF);
-  if (logic.lights.lights_on) {
-    logic.lights.off();
-  } else {
-    logic.lights.on();
-  }
-}
-
-void debug(int) {
-  logic.serial.print("toggling light sensor debugging...%s", CRLF);
-  logic.lightsensors.debugReadings = !logic.lightsensors.debugReadings;
-}
-
 void setup() {
   logic.setup();
-  logic.serial.print("Museum Mummy by kevinc...\n");
-  Serial.println(getFullVersion("museum-mummy"));
+  logic.serial.print("Museum Cabinet Pedestal by kevinc...\n");
+  Serial.println(getFullVersion("museum-cabinet-pedestal"));
 
   logic.serial.registerCommand(SerialCommand("status", 's', &status, "status", "gets the status of device"));
-  logic.serial.registerCommand(SerialCommand("solve",  'v', &solve,  "solve",  "force a puzzle solve of the device"));
-  logic.serial.registerCommand(SerialCommand("lights", 'l', &lights, "lights", "toggle lights on and off"));
-  logic.serial.registerCommand(SerialCommand("debug",  'x', &debug,  "debug",  "debug sensors"));
+  logic.serial.registerCommand(SerialCommand("solve",  'v', &solve,  "solve",  "force a solve and open the cabinet"));
   logic.serial.registerCommand(SerialCommand("reboot", 'r', &reboot, "reboot", "software reboot the device"));
 
   logic.serial.printHelp();
@@ -51,28 +35,3 @@ void setup() {
 void loop() {
   logic.handle();
 }
-
-//print("Opening device...%s", CRLF);
-//print("Closing device...%s", CRLF);
-//print("Turning all lights on...%s", CRLF);
-//print("Turning lights off...%s", CRLF);
-// print("Playing sound track...%s", CRLF);
-// print("Stoping sound track...%s", CRLF);
-// print("Stoping actuator...%s", CRLF);
-// print("Preloading actuator...%s", CRLF);
-// print("Toggling debug mode...%s", CRLF);
-// print("Triggering solved...%s", CRLF);
-
-// PREVIOUS COMMANDS ---------------------------
-// ---------------------------------------------
-//  open,o       => logic.actuator.open()
-//  close,c      => logic.actuator.close()
-//  lightson,l   => logic.lights.on()
-//  lightsall,a  => logic.lights.allOn()
-//  lightsoff,f  => logic.lights.off()
-//  play,p       => logic.sound.play()
-//  playstop,y   => logic.sound.stop()
-//  acstop,t     => logic.actuator.stop()
-//  preload,e    => logic.actuator.preload()
-//  solved,z     => logic.solved()
-//  status,s     => logic.printVariables()
